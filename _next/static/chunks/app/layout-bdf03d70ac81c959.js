3dd70325405a9081618ed657085c5995ad193ab6
Inna Dmytrenko <inna.shalun@gmail.com>
2025-03-25 16:46:45 +0100

    Updates

---
+++ _next/static/chunks/app/layout-bdf03d70ac81c959.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2982:()=>{},6025:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2982,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[107,937,350,358],()=>s(6025)),_N_E=e.O()}]);
